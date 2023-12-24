42858f53d24f062f6ce8e0fd382b1cc7b1f8c964
Dinesh9098 <151295073+Dinesh9098@users.noreply.github.com>
2023-12-24 07:34:04 +0530

    Update info.py

--- info.py
+++ info.py
@@ -53,7 +53,7 @@ DATABASE_NAME = environ.get('DATABASE_NAME', "vkbotsthalpathy")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
-VERIFY = bool(environ.get('VERIFY', False))
+VERIFY = bool(environ.get('VERIFY', True))
 SHORTLINK_URL = environ.get('SHORTLINK_URL', 'onepagelink.in')
 SHORTLINK_API = environ.get('SHORTLINK_API', '8b05406c954d2e76788d71308f74fdd20b4c4cbb')
 IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', True))
